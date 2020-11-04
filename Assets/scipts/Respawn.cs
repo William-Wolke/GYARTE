d9b88c86e3d256e7c99af49b3a3c65d3609a40b0
Teknokz <60339592+Teknokz@users.noreply.github.com>
2020-11-04 12:35:47 +0100

    Teleporter progress

---
+++ Assets/scipts/Respawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private Transform teleport;
+
+    void Start()
+    {
+        // Vector2 baguette = 
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger is running");
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("player hit trigger");
+            other.transform.position = teleport.position;
+        }
+    }
+}
