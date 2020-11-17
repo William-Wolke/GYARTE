13be01713017d0ac0984dca8e81b4b33e262db61
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2020-11-17 14:38:48 +0100

    scritp

---
+++ Assets/scipts/GameScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 15;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
