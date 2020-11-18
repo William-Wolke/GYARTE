5e5fc96c9a5ff9c2f653ab0683745d2ea2529f36
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2020-11-18 12:17:35 +0100

    FPs

--- Assets/scipts/GameScript.cs
+++ Assets/scipts/GameScript.cs
@@ -8,7 +8,8 @@ public class GameScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 15;
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
