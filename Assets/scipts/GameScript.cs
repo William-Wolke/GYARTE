d503d4f6454ed0d693f8c3b77c8ab0c6ec164c24
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2020-12-08 15:02:24 +0100

    coola bananer

--- Assets/scipts/GameScript.cs
+++ Assets/scipts/GameScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Net.Mime;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,13 +8,13 @@ using System.IO;
 
 public class GameScript : MonoBehaviour
 {
-    private float totalTime;
+    //private float totalTime;
     // Start is called before the first frame update
     void Start()
     {
         QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 30;
-        
+        Application.targetFrameRate = 15;
+        Debug.Log("Despacito");
     }
 
     // Update is called once per frame
@@ -22,7 +23,7 @@ public class GameScript : MonoBehaviour
         if(1==1) //det ska stå om man vunnit här.
         {
             Data spelData = new Data();
-            totalTime = Time.time;
+            //totalTime = Time.time;
             
 
         }
