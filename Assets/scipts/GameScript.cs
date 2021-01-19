ee0e6f48d20ae46f7c15be060209ff6291c78b94
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2021-01-19 08:26:10 +0100

    mer data

--- Assets/scipts/GameScript.cs
+++ Assets/scipts/GameScript.cs
@@ -21,7 +21,7 @@ public class GameScript : MonoBehaviour
     void Start()
     {
         QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 30;
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
